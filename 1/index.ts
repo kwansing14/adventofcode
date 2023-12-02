91f2662a140e5f0ba4b8629bc3658bfe87bb7fc2
kwansing14 <kwansing@hotmail.sg>
2023-12-02 15:33:00 +0800

    feat: day1 aoc done

---
+++ 1/index.ts
@@ -0,0 +1,16 @@
+const file = Bun.file('./input.txt');
+const text = await file.text();
+const texts = text.split('\n');
+
+let sum = 0;
+
+for (let i = 0; i < texts.length; i++) {
+  texts[i] = texts[i].replace(/[^\d]/g, '');
+  const firstNumber = Number(texts[i].at(0)) || 0;
+  const secondNumber = Number(texts[i].at(-1)) || 0;
+  const total = firstNumber * 10 + secondNumber;
+  sum += total;
+}
+
+// answer
+console.log(sum);
