823945c300072e7ddd80e9e0c946d6dcd32938a7
Kwansing_Ng <kwansing_ng@singaporeair.com.sg>
2023-12-07 11:57:55 +0800

    feat: day6 part1 solved

--- 6/partone.ts
+++ 6/partone.ts
@@ -1,5 +1,67 @@
-const file = Bun.file("./example.txt");
-const text = await file.text();
-const texts = text.split("\n\n");
+import { readTxt, removeUndefinedFromArray } from "../common/utils";
 
-console.log(texts);
+const getTimeDistance = (texts: string[]) => {
+  const time = texts[0].split(" ");
+  const newTime = removeUndefinedFromArray(time);
+  const distance = texts[1].split(" ");
+  const newDistance = removeUndefinedFromArray(distance);
+
+  return { time: newTime, distance: newDistance };
+};
+
+const getNumberOfTracks = (texts: string[]) => {
+  const time = texts[0].split(" ");
+  const newTime = removeUndefinedFromArray(time);
+  return newTime.length - 1;
+};
+
+const getTrack = (res: { time: string[]; distance: string[] }, num: number) => {
+  return {
+    time: Number(res.time[num + 1]),
+    distance: Number(res.distance[num + 1]),
+  };
+};
+
+const trackSuccess = (
+  holdTime: number,
+  selectedTrack: { time: number; distance: number }
+) => {
+  const speed = holdTime * 1;
+  const { distance: distanceTarget, time } = selectedTrack;
+
+  const distanceTraveled = speed * (time - holdTime);
+
+  if (distanceTraveled > distanceTarget) return true;
+  return false;
+};
+
+const allPossibleTracksSuccess = (track: {
+  time: number;
+  distance: number;
+}) => {
+  let count = 0;
+  for (let i = 0; i < track.time; i++) {
+    const isSuccess = trackSuccess(i, track);
+    if (isSuccess) count++;
+  }
+  return count;
+};
+
+const main = async (url: string) => {
+  let ans = 1;
+  const texts = await readTxt(url);
+  const timeDistance = getTimeDistance(texts);
+  const numberOfTracks = getNumberOfTracks(texts);
+
+  for (let i = 0; i < numberOfTracks; i++) {
+    const track = getTrack(timeDistance, i);
+    const count = allPossibleTracksSuccess(track);
+    console.log(count);
+    ans = ans * count;
+  }
+  return ans;
+};
+
+console.log("start");
+const ans = await main("./input.txt");
+console.log("ans:", ans);
